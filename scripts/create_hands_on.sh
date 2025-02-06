1357fdbb43ef2878fdab2b2551b8fa57f00216c4
Tom Bland <tom_bland@hotmail.co.uk>
2025-02-06 16:43:10 +0000

    Update

--- scripts/create_hands_on.sh
+++ scripts/create_hands_on.sh
@@ -38,8 +38,8 @@ convert_notebook $notebook
 notebooks=(MUSE_OS/docs/user-guide/*.ipynb)
 for notebook in "${notebooks[@]}"; do
     html_file=$(convert_notebook "$notebook")
-    update_relative_links $html_file "https://muse-os.readthedocs.io/en/v1.2.1/user-guide/"
-    update_github_links $html_file "v1.2.1"
+    update_relative_links $html_file "https://muse-os.readthedocs.io/en/v1.3.2/user-guide/"
+    update_github_links $html_file "v1.3.2"
 done
 
 # Specify tutorial order
